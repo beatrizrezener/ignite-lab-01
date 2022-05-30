ff3a5afad65e4301d2ade71fa972004bbb41cbce
Beatriz Rezener <beatrizrezener@gmail.com>
2022-05-30 00:58:05 -0300

    purchases: createPurchase mutation and customer resolver

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,8 +5,10 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
+import { CustomersService } from '../services/customers.service';
 import { ProductsService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
@@ -22,9 +24,11 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
   providers: [
     ProductsResolver,
     PurchasesResolver,
+    CustomersResolver,
 
     ProductsService,
     PurchasesService,
+    CustomersService,
   ],
 })
 export class HttpModule {}
--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -18,4 +18,34 @@ export class PurchasesService {
       },
     });
   }
+
+  listAllFromCustomer(customerId: string) {
+    return this.prisma.purchase.findMany({
+      where: {
+        customerId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ customerId, productId }: CreatePurchaseParams) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found.');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customerId,
+        productId,
+      },
+    });
+  }
 }
