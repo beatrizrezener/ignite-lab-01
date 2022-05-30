bd23a6922a4fd4fb17056e146e0664c0d67b4a44
Beatriz Rezener <beatrizrezener@gmail.com>
2022-05-29 22:33:08 -0300

    web: redirect user based on session

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,17 +1,26 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { getSession } from "@auth0/nextjs-auth0"
+import { GetServerSideProps } from "next"
 
 export default function Home() {
-  const { user } = useUser()
-  
-  return (
-    <div>
-      <h1>Hello World</h1>
+  return null;
+}
 
-      <pre>
-        { JSON.stringify(user, null, 2) }
-      </pre>
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
 
-      <a href="/api/auth/login">Login</a>
-    </div>
-  )
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false,
+      }
+    }
+  } else {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      }
+    }
+  }
 }
